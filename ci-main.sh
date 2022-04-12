b24b01143768efdeeed22874b4f83525a250609a
Jeremy Busk <jeremybusk@gmail.com>
2022-04-12 16:42:39 -0600

    test

--- ci-main.sh
+++ ci-main.sh
@@ -4,6 +4,8 @@ release=6.0.3-ubuntu
 repo=uvoo/zabbix-server-pgsql
 tag=${repo}:${release}
 echo "Build and push docker container to Dockerhub."
+echo myuser $DOCKERHUB_USERNAME
+exit
 docker build --tag ${tag} .
 echo $DOCKERHUB_USERTOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push ${tag}
