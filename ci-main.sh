6b113fe1c26587b18a8c3c900de3408d3573cfff
Jeremy Busk <jeremybusk@gmail.com>
2022-04-12 16:37:51 -0600

    fix cred name

--- ci-main.sh
+++ ci-main.sh
@@ -2,9 +2,9 @@
 set -e
 release=6.0.3-ubuntu
 repo=uvoo/zabbix-server-pgsql
-tag=$repo:${release}
-
-echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
+tag=${repo}:${release}
+echo "Build and push docker container to Dockerhub."
 docker build --tag ${tag} .
+echo $DOCKERHUB_USERTOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push ${tag}
 docker logout
